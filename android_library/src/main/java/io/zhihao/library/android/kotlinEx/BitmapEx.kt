c160a54ec7f33a0a5a0a75d317c1eefdbe3c074a
zhihaofans <zhihaofans@hotmail.com>
2019-05-16 22:05:21 +0800

    Add kotlinEx/BitmapEx.kt

---
+++ android_library/src/main/java/io/zhihao/library/android/kotlinEx/BitmapEx.kt
@@ -0,0 +1,14 @@
+package io.zhihao.library.android.kotlinEx
+
+import android.graphics.Bitmap
+import io.zhihao.library.android.util.EncodeUtil
+
+/**
+ * 在此写用途
+
+ * @author: zhihaofans
+
+ * @date: 2019-05-16 22:00
+
+ */
+fun Bitmap.getBase64String(): String? = EncodeUtil.bitmapToBase64String(this)
