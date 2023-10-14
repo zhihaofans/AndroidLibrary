a04ed32d7160123f4cbb7f21a0145dc628b4af55
zhihaofans <zhihaofans@hotmail.com>
2023-10-14 22:19:51 +0800

    AlertUtil:新增showInputAlert，showListAlert不能点击外面取消

--- android_library/src/main/java/io/zhihao/library/android/util/AlertUtil.kt
+++ android_library/src/main/java/io/zhihao/library/android/util/AlertUtil.kt
@@ -3,9 +3,14 @@ package io.zhihao.library.android.util
 import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
+import android.text.InputType
+
+import android.widget.EditText
+
 
 class AlertUtil(context: Context) {
     private val _context = context
+
     fun showListAlert(
         title: String,
         itemList: Array<String>,
@@ -26,7 +31,29 @@ class AlertUtil(context: Context) {
             }
 
         val dialog: AlertDialog = builder.create()
+        dialog.setCanceledOnTouchOutside(true)
         dialog.show()
         return dialog
+    }
+
+    fun showInputAlert(
+        title: String, inputText: String? = "",
+        onClick: (text: String, dialog: DialogInterface) -> Unit
+    ) {
+        val builder = AlertDialog.Builder(_context)
+        builder.setTitle(title)
+        val input = EditText(_context)
+        input.inputType = InputType.TYPE_CLASS_TEXT
+        input.setText(inputText)
+        builder.setView(input)
+        builder.setPositiveButton("OK") { dialog, which ->
+            onClick.invoke(
+                input.text.toString(),
+                dialog
+            )
+            dialog.dismiss()
+        }
+        builder.setNegativeButton("Cancel") { dialog, which -> dialog.cancel() }
+        builder.show()
     }
 }
